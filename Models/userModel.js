bcfff8ab6a32764f7175db050256151253a092ac
Sourav Singha <singhasourav632@gmail.com>
2024-07-15 10:43:22 +0530

    model setup and schema creation

---
+++ Models/userModel.js
@@ -0,0 +1,19 @@
+const mongoose=require('mongoose');
+
+const userSchema=new mongoose.Schema(
+    {
+        _id: mongoose.Schema.Types.ObjectId,
+        firstName:{type: String, required: true},
+        lastName:{type: String, required: true},
+        address:{type: String},
+        city:{type: String, required: true},
+        state:{type: String, required: true},
+        zipcode:{type: Number, required: true},
+        email:{type: email, required:true},
+        password:{type: String, required: true}
+    }
+)
+
+const userModel=mongoose.model('user', userSchema);
+
+module.exports=userModel;
